eacbcfec1c54a86b94f30a6023eb212fe9de4a4b
Yakov Krasner <yakov.krasner@yandex.ru>
2022-09-10 21:40:01 +0700

    Initial commit_Akkerman

---
+++ Akkerman/Program.cs
@@ -0,0 +1,10 @@
+﻿int Akkerman (int m, int n)
+{
+if (m == 0) return n+1;
+else 
+    {
+        if (m > 0 && n == 0) return Akkerman (m - 1,1);
+            else return Akkerman (m-1, Akkerman (m, n - 1));
+    }
+}
+Console.WriteLine (Akkerman (3,2));
